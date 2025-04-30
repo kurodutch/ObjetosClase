ff01cbe04eaf11c3bc5952a1962a158fcab81376
kurodutch <34357575+kurodutch@users.noreply.github.com>
2025-04-30 00:43:46 -0400

    Clases
    se agregan más clases, junto a sus getter y setter, y ejemplos de herencia y polimorfismo.

--- ObjetosClase/Program.cs
+++ ObjetosClase/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
 
 namespace ObjetosClase
 {
@@ -54,7 +55,7 @@ namespace ObjetosClase
             this.Nombre = nombre;
             this.edad = edad;
             this.direccion = direccion;
-            this.teléfono= telefono;
+            this.teléfono = telefono;
             this.email = email;
         }
 
@@ -67,16 +68,125 @@ namespace ObjetosClase
 
 
     }
-        internal class Program
-        {
-            static void Main(string[] args)
-            {
-                Persona persona = new Persona();
-                Persona persona2 = new Persona("Pedro", 25, "palalal", "936117842", "pepito@gmail"); 
-                persona.Nombre = "Juanito";
-                persona2.MostrarInformacionCompleta();
-                persona.MostrarInformacionCompleta();
 
-            }
+
+    class Animal
+    {
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+        public Animal(string nombre, int edad)
+        {
+            Nombre = nombre;
+            Edad = edad;
         }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"El animal es {Nombre} y la edad es {Edad}");
+        }
+
+        public virtual void PedirComida()
+        {
+            Console.WriteLine($"{Nombre} está pidiendo comida");
+        }
+    }
+    class Perro : Animal // al colocar ":" indica que la clase Perro está heredando las características de la calse Animal.
+    {
+        public string Raza { get; set; }
+        public Perro(string nombre, int edad, string raza) : base(nombre, edad)
+        {
+            this.Raza = raza;
+        }
+
+
+        public void Ladrar()
+        {
+            Console.WriteLine($"{Nombre} está ladrando");
+        }
+
+        public override void PedirComida()
+        {
+            //base.PedirComida();
+            Console.WriteLine($"{Nombre}(perro) mueve la cola y salta para pedir comida");
+        }
+
+    }
+
+    class Gato : Animal
+    {
+        public bool EsDomestico { get; set; }
+        public Gato(string nombre, int edad, bool domestico) : base(nombre, edad)
+        {
+            EsDomestico = domestico;
+        }
+
+        public void Maullar()
+        {
+            Console.WriteLine($"{Nombre} está maullando");
+        }
+
+        public override void PedirComida()
+        {
+            //base.PedirComida();
+            Console.WriteLine($"{Nombre}(gato) exige comida con sus maullidos insistentes");
+        }
+    }
+
+    class Pajaro : Animal
+    {
+        public bool pajaroCanta { get; set; }
+
+        public Pajaro(string nombre, int edad, bool canta) : base(nombre, edad)
+        {
+            pajaroCanta = canta;
+
+        }
+
+    }
+    
+    class Reptil: Animal
+    {
+        public bool Escamoso { get; set; }
+        public Reptil (string nombre, int edad, bool escamoso): base (nombre, edad)
+        {
+            Escamoso = escamoso;
+        }
+
+        public override void PedirComida() 
+        {
+            Console.WriteLine($"{Nombre} está pidiendo comida como buen reptil que es");        
+        
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal miAnimal = new Animal("Canela", 3);
+            miAnimal.MostrarInformacion();
+            miAnimal.PedirComida();
+
+            Perro miPerro = new Perro("Ronaldo", 15, "cocker");
+            miPerro.MostrarInformacion();
+
+            Gato miGato = new Gato("Cucho", 1, true);
+            miGato.MostrarInformacion();
+
+            Pajaro miPajaro = new Pajaro("pepe", 4, true);
+            miPajaro.MostrarInformacion();
+
+            Reptil miReptil = new Reptil("Luna", 13, true);
+
+            //miPerro.PedirComida();
+            //miGato.PedirComida();
+
+            Animal[] animales = { miPerro, miGato, miPajaro, miReptil };
+            foreach (Animal animal in animales)
+            {
+                animal.PedirComida();
+            }
+
+        }
+    }
 }
